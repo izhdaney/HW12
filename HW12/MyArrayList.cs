3ac1004ba3ebd462fc94a402d863045ad0e65d24
izhdaney <79247431+izhdaney@users.noreply.github.com>
2021-04-19 12:11:16 +0300

    no message

---
+++ HW12/MyArrayList.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW12
+{
+    class MyArrayList : IList
+    {
+
+        private object[] _myarrayList;
+
+        public MyArrayList()
+        {
+            _myarrayList = new object[0];
+    }
+
+        public int Add(object value)
+        {
+            object[] tmp = new object[_myarrayList.Length + 1];
+            for (int i = 0; i < _myarrayList.Length; i++)
+            {
+                tmp[i] = _myarrayList[i];
+            }
+
+            tmp[tmp.Length - 1] = value;
+
+            _myarrayList = tmp;
+
+            return tmp.Length - 1;
+        }
+
+        public void Clear()
+        {
+            _myarrayList = new object[0];
+        }
+
+        public bool Contains(object value)
+        {
+  
+            for (int i = 0; i < Count; i++)
+            {
+                if (_myarrayList[i] == value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _myarrayList.GetEnumerator();
+        }
+
+        public int IndexOf(object value)
+        {
+            for (int i = 0; i < _myarrayList.Length; i++)
+            {
+                if (_myarrayList[i] == value)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void Insert(int index, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                return _myarrayList[index];
+            }
+            set
+            {
+                _myarrayList[index] = value;
+            }
+        }
+
+        public bool IsFixedSize
+        {
+            get { return false; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public int Count 
+        { 
+            get { return _myarrayList.Length; }
+        }
+
+        public bool IsSynchronized
+        {
+            get { return false; }
+        }
+
+        public object SyncRoot
+        {
+            get { return this; }
+        }
+    }
+}
--- HW12/Program.cs
+++ HW12/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace HW12
 {
@@ -6,7 +7,74 @@ namespace HW12
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ArrayList arr = new ArrayList();
+
+            Console.WriteLine(arr.Count);
+            Console.WriteLine(arr.IsFixedSize);
+            Console.WriteLine(arr.IsReadOnly);
+            Console.WriteLine(arr.IsSynchronized);
+            Console.WriteLine(arr.SyncRoot);
+
+            Console.WriteLine(arr.Add(6));
+            Console.WriteLine(arr.Add(5));
+            Console.WriteLine(arr.Add(8));
+
+            arr.Clear();
+
+            foreach (var o in arr)
+            {
+                Console.Write("{0}\t", o);
+            }
+
+            Console.WriteLine(arr.Add(6));
+            Console.WriteLine(arr.Add(5));
+            Console.WriteLine(arr.Add(8));
+
+            Console.WriteLine(arr.Contains(5));
+            Console.WriteLine(arr.Contains(4));
+
+
+
+            MyArrayList myArrayList = new MyArrayList();
+
+            Console.WriteLine("\n My Collection\n");
+
+            //Console.WriteLine(myArrayList.Count);
+            //Console.WriteLine(myArrayList.IsFixedSize);
+            //Console.WriteLine(myArrayList.IsReadOnly);
+            //Console.WriteLine(myArrayList.IsSynchronized);
+            //Console.WriteLine(myArrayList.SyncRoot);
+
+
+            //Console.WriteLine(myArrayList.Add(6));
+            //Console.WriteLine(myArrayList.Add(5));
+            //Console.WriteLine(myArrayList.Add(8));
+
+
+
+            //Console.WriteLine(myArrayList.GetEnumerator());
+
+            myArrayList.Clear();
+
+            foreach (var o in myArrayList)
+            {
+                Console.Write("{0}\t", o);
+            }
+
+
+            Console.WriteLine(myArrayList.Add(6));
+            Console.WriteLine(myArrayList.Add(5));
+            Console.WriteLine(myArrayList.Add(8));
+
+            Console.WriteLine(myArrayList.Contains(5));
+            Console.WriteLine(myArrayList.Contains(4));
+
+            Console.WriteLine(myArrayList.IndexOf(6));
+
+            foreach (var o in myArrayList)
+            {
+                Console.Write("{0}\t", o);
+            }
         }
     }
 }
